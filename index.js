97113bce7d27af873b295ede8884c65fc08fd52f
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-12-30 19:23:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const fs = require("fs").promises
 const fss = require("fs");
 const { Configuration, OpenAIApi } = require("openai");
 const csv = require('csv-parser')
-//const ufs = require("url-file-size");
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -31,46 +30,44 @@ app.post('/create-open-ai-model', async (req, res) => {
     if (cond == true) {
 
       await axios.get(data['csvUrl'])
-      .then((response) => {
+        .then((response) => {
 
-        const csv_character_length = response.data.length
-        const num_records = response.data.split('\n').length
-        const fileSize = Math.floor(csv_character_length/1000)
-        const n_epochs = data['n_epochs'] ? data['n_epochs'] : 2
-        var count = (csv_character_length / 4) * n_epochs
-  
-        var condi = true
-        switch (data['model']) {
-          case 'davinci':
-            var n = 0.00003
-            break;
-          case 'curie':
-            var n = 0.000003
-            break;
-          case 'babbage':
-            var n = 0.0000006
-            break;
-          case 'ada':
-            var n = 0.0000004
-            break;
-          default:
-            condi = false
-            res.status(500).send({ 'error': "Model is incorrect" })
-            break;
-        }
-  
-        if (condi == true) {
-          count = count * n
-          count = count.toFixed(2)
-          if (count < 0.01) {
-            res.status(200).send({ "estimate": `<$0.01` , "num_records": num_records, "num_chars": csv_character_length, "file_size": fileSize })
-          } else {
-            res.status(200).send({ "estimate": `~$${count}`, "num_records": num_records, "num_chars": csv_character_length, "file_size": fileSize })
+          const csv_character_length = response.data.length
+          const num_records = response.data.split('\n').length
+          const fileSize = csv_character_length / 1000
+          const n_epochs = data['n_epochs'] ? data['n_epochs'] : 2
+          var count = (csv_character_length / 4) * n_epochs
+
+          var condi = true
+          switch (data['model']) {
+            case 'davinci':
+              var n = 0.00003
+              break;
+            case 'curie':
+              var n = 0.000003
+              break;
+            case 'babbage':
+              var n = 0.0000006
+              break;
+            case 'ada':
+              var n = 0.0000004
+              break;
+            default:
+              condi = false
+              res.status(500).send({ 'error': "Model is incorrect" })
+              break;
           }
-        }
 
-      })
-
+          if (condi == true) {
+            count = count * n
+            count = count.toFixed(2)
+            if (count < 0.01) {
+              res.status(200).send({ "estimate": `<$0.01`, "num_records": num_records, "num_chars": csv_character_length, "file_size": fileSize })
+            } else {
+              res.status(200).send({ "estimate": `~$${count}`, "num_records": num_records, "num_chars": csv_character_length, "file_size": fileSize })
+            }
+          }
+        })
     }
 
   } else {
@@ -86,68 +83,62 @@ app.post('/create-open-ai-model', async (req, res) => {
         var arr = []
         var charLength = 0
 
-        await axios.get(data['csvUrl'], { responseType: "stream",})
-        .then(async(response) => {
-          
-          await response.data
-            .pipe(csv())
-            .on("data", function (row) {
-              arr.push(row);
-              charLength += JSON.stringify(Object.values(row)).replaceAll('[', '').replaceAll(']', '').length
-            })
-            .on("end", async function () {
-          
-              var colOne = Object.keys(arr[0])[0]
-              var colTwo = Object.keys(arr[0])[1]   
+        await axios.get(data['csvUrl'], { responseType: "stream", })
+          .then(async (response) => {
 
-              const arr2 = arr.map((item) => (
-                { 
-                  prompt: item[colOne] + "\n\n###\n\n",
-                  completion: item[colTwo] + "###"
-                }
-              ))
+            await response.data
+              .pipe(csv())
+              .on("data", function(row) {
+                arr.push(row);
+                charLength += JSON.stringify(Object.values(row)).replaceAll('[', '').replaceAll(']', '').length
+              })
+              .on("end", async function() {
 
-              // const fileSize = await ufs(data['csvUrl'])
-              //   .then(async (size) => {
-              //     return Math.floor(size/1000)
-              //   })
-              //   .catch(console.error);
+                var colOne = Object.keys(arr[0])[0]
+                var colTwo = Object.keys(arr[0])[1]
 
-              var fileSize = await axios.get(data['csvUrl'])
-                .then((response) => {
-                  return Math.floor(response.data.length/1000)
-                })
+                const arr2 = arr.map((item) => (
+                  {
+                    prompt: item[colOne] + "\n\n###\n\n",
+                    completion: item[colTwo] + "###"
+                  }
+                ))
 
-              const filename = uuidv4() + '.jsonl';
-              await fs.writeFile(filename, JSON.stringify(arr2).replaceAll('[', '').replaceAll(']', '').replaceAll('},{', '}\n{'))
+                var fileSize = await axios.get(data['csvUrl'])
+                  .then((response) => {
+                    return response.data.length / 1000
+                  })
 
-              const configuration = new Configuration({
-                apiKey: data['apiKey'],
+                const filename = uuidv4() + '.jsonl';
+                await fs.writeFile(filename, JSON.stringify(arr2).replaceAll('[', '').replaceAll(']', '').replaceAll('},{', '}\n{'))
+
+                const configuration = new Configuration({
+                  apiKey: data['apiKey'],
+                });
+                const openai = new OpenAIApi(configuration);
+                await openai.createFile(
+                  fss.createReadStream(filename),
+                  "fine-tune"
+                ).then(async responses => {
+
+                  await openai.createFineTune({
+                    training_file: responses.data.id,
+                    model: data['model'] ? data['model'] : 'davinci',
+                    n_epochs: data['n_epochs'] ? data['n_epochs'] : 2,
+                    suffix: data['model_name'] ? data['model_name'] : 'my-model'
+                  }).then(response2 => {
+                    res.status(200).send({ "message": "Success", "id": response2.data.id, "num_records": arr.length + 1, "num_chars": charLength, "file_size": fileSize })
+                  }).catch(err => { res.status(401).send({ 'error': err.message, message: 'Creation failed' }) });
+
+                }).catch(err => { res.status(401).send({ 'error': err.message, message: "Invalid API key" }) });
+
+                await fs.unlink(filename)
+              })
+              .on("error", function(error) {
+                res.status(response.status).send({ 'error': response.error, 'message': "error reading CSV URL" })
               });
-              const openai = new OpenAIApi(configuration);
-              await openai.createFile(
-                fss.createReadStream(filename),
-                "fine-tune"
-              ).then(async responses => {
-
-                await openai.createFineTune({
-                  training_file: responses.data.id,
-                  model: data['model_name'] ? `${data['model']}:${data['model_name']}` : data['model'],
-                  n_epochs: data['n_epochs'] ? data['n_epochs'] : 2
-                }).then(response2 => {
-                  res.status(200).send({ "message": "Success", "id": response2.data.id, "num_records": arr.length+1, "num_chars": charLength, "file_size": fileSize })
-                }).catch(err => { res.status(401).send({ 'error': err.message, 'message': 'Creation failed' }) });
-
-              }).catch(err => { res.status(401).send({ 'error': err.message, message: "Invalid API key" }) });
-
-              await fs.unlink(filename)
-            })
-            .on("error", function (error) {
-              res.status(response.status).send({ 'error': response.error, 'message': "error reading CSV URL"  })
-            });
-        })   
-        .catch(err => { res.status(403).send({ 'error': "CSV URL unreadable" }) });
-        
+          })
+          .catch(err => { res.status(403).send({ 'error': "CSV URL unreadable" }) });
       }
     }
   }
@@ -155,57 +146,57 @@ app.post('/create-open-ai-model', async (req, res) => {
 
 app.post('/open-ai-models', async (req, res) => {
 
-    const data2 = req.body
-    const dataKeys2 = Array.from(Object.keys(data2))
+  const data2 = req.body
+  const dataKeys2 = Array.from(Object.keys(data2))
 
-    if (dataKeys2.includes("apiKey")){
+  if (dataKeys2.includes("apiKey")) {
 
-      try {
+    try {
 
-          const configuration = new Configuration({
-            apiKey: data2['apiKey'],
-          });
-          const openai = new OpenAIApi(configuration);
-          await openai.listFineTunes().then(async latest_response => {
+      const configuration = new Configuration({
+        apiKey: data2['apiKey'],
+      });
+      const openai = new OpenAIApi(configuration);
+      await openai.listFineTunes().then(async latest_response => {
 
-            var result = latest_response.data.data.map( (item) => {
-              var utcSeconds = item.created_at;
-              var date = new Date(0);
-              date.setUTCSeconds(utcSeconds);
+        var result = latest_response.data.data.map((item) => {
+          var utcSeconds = item.created_at;
+          var date = new Date(0);
+          date.setUTCSeconds(utcSeconds);
 
-              if (item.status == "failed") {
-                return {
-                  id: item.id,
-                  n_epochs: item.hyperparams.n_epochs,
-                  model: item.model,
-                  status: item.status,
-                  created_at: date.toDateString(),
-                  file_error: item.training_files[0].status_details,
-                  fine_tuned_model: item.fine_tuned_model
-                };
-              } else {
-                return {
-                  id: item.id,
-                  n_epochs: item.hyperparams.n_epochs,
-                  model: item.model,
-                  status: item.status,
-                  created_at: date.toDateString(),
-                  fine_tuned_model: item.fine_tuned_model
-                };
-              }
+          if (item.status == "failed") {
+            return {
+              id: item.id,
+              n_epochs: item.hyperparams.n_epochs,
+              model: item.model,
+              status: item.status,
+              created_at: date.toDateString(),
+              file_error: item.training_files[0].status_details,
+              fine_tuned_model: item.fine_tuned_model
+            };
+          } else {
+            return {
+              id: item.id,
+              n_epochs: item.hyperparams.n_epochs,
+              model: item.model,
+              status: item.status,
+              created_at: date.toDateString(),
+              fine_tuned_model: item.fine_tuned_model
+            };
+          }
 
-            });
-            for (let index = 0; index < result.length; index++) {const elem = result[index]}
-            res.status(200).send(result.reverse())
-          }).catch(err => { res.status(400).send({ 'error': err.message}) })
+        });
+        for (let index = 0; index < result.length; index++) { const elem = result[index] }
+        res.status(200).send(result.reverse())
+      }).catch(err => { res.status(400).send({ 'error': err.message }) })
 
-      } catch(err) {
-          res.status(401).send({ 'error': "Invalid Credentials (Incorrect API Key)" })
-      }
-
-    } else {
-        res.status(500).send({ 'error': "No API key found" })
+    } catch (err) {
+      res.status(401).send({ 'error': "Invalid Credentials (Incorrect API Key)" })
     }
+
+  } else {
+    res.status(500).send({ 'error': "No API key found" })
+  }
 })
 
 app.listen(port, () => {
